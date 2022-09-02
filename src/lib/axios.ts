b6a6da47af631ed00097eb8d9cdce0e6088cf023
naysw <naysoewin9@gmail.com>
2022-09-02 10:52:45 +0630

    added axios instance

---
+++ src/lib/axios.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { API_URI } from "../config/constants";
+
+/**
+ * create new axios instance for client side fetching
+ */
+const axiosClient = axios.create({
+  baseURL: API_URI,
+  // headers: DEFAULT_HEADERS,
+});
+
+axiosClient.interceptors.request.use((config) => {
+  if (!config.headers) return;
+
+  // config.headers["Authorization"] = `Bearer ${window.localStorage.getItem(
+  //   ACCESS_TOKEN
+  // )}`;
+
+  return config;
+});
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    switch (error.response.status) {
+      // case HttpStatus.UNAUTHORIZED:
+      //   Cookies.remove(IS_LOGGED_IN_KEY);
+      //   window.localStorage.removeItem(ACCESS_TOKEN);
+      //   break;
+
+      default:
+        return Promise.reject(error.response.data);
+    }
+  },
+);
+
+export default axiosClient;
